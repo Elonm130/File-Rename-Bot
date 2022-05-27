749430521ff4ecab5438632dc8db2b4a78735a64
Elonm130 <105444252+Elonm130@users.noreply.github.com>
2022-05-27 14:46:49 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -2,31 +2,24 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
 import asyncio
 import json
 import math
 import os
 import time
-
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-
 # the Strings used for this "thing"
 from translation import Translation
-
 import pyrogram
 from pyrogram import filters
 from pyrogram import Client
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
-
-
 @pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
 async def text(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
@@ -48,18 +41,22 @@ async def text(bot, update):
               ])
             )
             return
-           else:
+        else:
             await update.reply_text(Translation.START_TEXT.format(update.from_user.first_name),
         reply_markup=InlineKeyboardMarkup(
             [
-               
                 [
+                    InlineKeyboardButton('Channel ', url='https://t.me/DevelopedBots'),
+                    InlineKeyboardButton('Support ', url='https://t.me/DevelopedBotz')
+                ],
+                [
+                    InlineKeyboardButton('Source ', url='https://github.com/DevelopedBots/FileRenameBot'),
                     InlineKeyboardButton('Source ', url='https://github.com/DevelopedBots/FileRenameBot')
-                  
                 ]
             ]
         ),
         reply_to_message_id=update.message_id
     )
             return 
-     
+
+ 
